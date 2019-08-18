8e2338578b1f0313d26d927c0a47b755bdc933a0
Sathwik Matsa <sathwikmatsa@gmail.com>
2019-08-18 19:33:16 +0530

    override and implement add, sub, neg operators in vec3 struct

--- src/vec3.rs
+++ src/vec3.rs
@@ -1,10 +1,12 @@
-#[derive(Debug)]
+use std::ops::{Add, Sub, Neg};
+
+#[derive(Debug, PartialEq, Copy, Clone)]
 enum Vec3Type {
     Point,
     Vector,
 }
 
-#[derive(Debug)]
+#[derive(Debug, PartialEq, Copy, Clone)]
 pub struct Vec3 {
     x: f32,
     y: f32,
@@ -45,5 +47,107 @@ impl Vec3 {
             z: z.value(),
             t: Vec3Type::Vector,
         }
+    }
+}
+
+// operator overloading
+
+impl Add for Vec3 {
+    type Output = Self;
+
+    fn add(self, other: Self) -> Self {
+
+        let output_t;
+        if self.t == Vec3Type::Point && other.t == Vec3Type::Point {
+            panic!("Cannot add a point to a point.");
+        } else if self.t == Vec3Type::Point {
+            // point + vector -> point
+            output_t = Vec3Type::Point;
+        } else {
+            // vector + vector -> vector
+            output_t = other.t;
+        }
+
+        Self {
+            x: self.x + other.x,
+            y: self.y + other.y,
+            z: self.z + other.z,
+            t: output_t,
+        }
+    }
+}
+
+impl Sub for Vec3 {
+    type Output = Self;
+
+    fn sub(self, other: Self) -> Self {
+
+        let output_t;
+        if self.t == Vec3Type::Point && other.t == Vec3Type::Point {
+            // p2 - p1 -> vector(p2-p1)
+            output_t = Vec3Type::Vector;
+        } else if self.t == Vec3Type::Point {
+            // point - vector -> point
+            output_t = Vec3Type::Point;
+        } else if self.t == Vec3Type::Vector && other.t == Vec3Type::Vector{
+            // vector - vector -> vector
+            output_t = Vec3Type::Vector;
+        } else {
+            panic!("Cannot subtract a point from a vector.");
+        }
+
+        Self {
+            x: self.x - other.x,
+            y: self.y - other.y,
+            z: self.z - other.z,
+            t: output_t,
+        }
+    }
+}
+
+impl Neg for Vec3 {
+    type Output = Self;
+
+    fn neg(self) -> Self {
+        Self {
+            x: -self.x,
+            y: -self.y,
+            z: -self.z,
+            t: self.t,
+        }
+    }
+}
+
+// unit tests
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn adding_vec3() {
+        let p = Vec3::point(3, -2, 5);
+        let v = Vec3::vector(-2, 3, 1);
+        let l = p + v;
+        assert_eq!(l, Vec3::point(1, 1, 6));
+    }
+    #[test]
+    fn subtracting_vec3() {
+        let p1 = Vec3::point(3, 2, 1);
+        let p2 = Vec3::point(5, 6, 7);
+        let v = Vec3::vector(-2, -4, -6);
+        assert_eq!(v, p1 - p2);
+
+        let v1 = Vec3::vector(5, 6, 7);
+        let p3 = Vec3::point(-2, -4, -6);
+        assert_eq!(p3, p1 - v1);
+
+        let v2 = Vec3::vector(3, 2, 1);
+        assert_eq!(v, v2 - v1);
+    }
+    #[test]
+    fn negating_vec3() {
+        let v1 = Vec3::vector(1, 2, 3);
+        let v2 = -v1;
+        assert_eq!(v2, Vec3::vector(-1, -2, -3));
     }
 }
