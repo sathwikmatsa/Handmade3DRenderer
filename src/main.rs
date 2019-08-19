cdbc2cb8232206ec7bba7b3232d7f1341b194598
Sathwik Matsa <sathwikmatsa@gmail.com>
2019-08-19 23:10:41 +0530

    draw projectile on canvas

--- src/main.rs
+++ src/main.rs
@@ -21,15 +21,16 @@ fn tick(e: &Environment, p: Projectile) -> Projectile{
 }
 
 fn main() {
-    let velocity_scaling_factor = 2;
-    let mut p = Projectile{position: Vec3::point(0, 1, 0), velocity: Vec3::vector(1, 1, 0).normalize() * velocity_scaling_factor};
+    let velocity_scaling_factor = 11.25;
+    let mut p = Projectile{position: Vec3::point(0, 1, 0), velocity: Vec3::vector(1.0, 1.8, 0.0).normalize() * velocity_scaling_factor};
     let e = Environment{gravity: Vec3::vector(0.0, -0.1, 0.0), wind: Vec3::vector(-0.01, 0.0, 0.0)};
 
+    let mut canvas = Canvas::new(900, 550);
+    let color = Color::new(1.0, 1.0, 1.0);
+
     while p.position.y > 0.0 {
+        canvas.write_pixel(p.position.x as u32, canvas.height - p.position.y as u32, color);
         p = tick(&e, p);
-        //println!("projectile pos: {:?}", p.position);
     }
-
-    println!("Virtual projectile reached ~{} units", p.position.x);
-
+    canvas.canvas_to_ppm("projectile.ppm".to_string());
 }
