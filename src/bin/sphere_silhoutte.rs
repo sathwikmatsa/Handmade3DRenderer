0637cc2e0d0d0b1343fbcbc9743356b726cd0a6f
Sathwik Matsa <sathwikmatsa@gmail.com>
2019-08-28 21:29:41 +0530

    draw silhoutte of a sphere

--- src/bin/sphere_silhoutte.rs
+++ src/bin/sphere_silhoutte.rs
@@ -7,6 +7,17 @@ fn main() {
     let canvas_size = 400;
     let mut canvas = Canvas::new(canvas_size, canvas_size);
     let color = Color::new(1.0, 0.0, 0.0);
+    //let origin = Vec3::point(0, 0, 0);
+    //let radius = 0.4 * width as f32;
+    //let mut clock_position = Matrix::translation(radius, 0.0, 0.0) * origin;
+    //let separation_angle : f32 = 2.0 * std::f32::consts::PI / 12.0;
+    //for _ in 0..12 {
+    //    // translate origin to center of canvas
+    //    let canvas_point = Matrix::translation(width as f32 / 2.0, height as f32 / 2.0 , 0.0) * clock_position;
+    //    canvas.write_pixel(canvas_point.x as u32, canvas_point.y as u32, color);
+    //    clock_position = Matrix::rotation_z(separation_angle) * clock_position;
+    //}
+    //canvas.canvas_to_ppm("analog_clock.ppm".to_string());
     let ray_origin = Vec3::point(0, 0, -5);
     let wall_z = 10.0;
     let wall_size = 7.0;
